21db82e625d5b118be4229e7d62689cdf24150e0
luft kluft <luftkluft@gmail.com>
2019-01-30 23:29:37 +0300

    Feature/dev: seed admin production

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,8 @@ if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password',
                     password_confirmation: 'password')
 end
+
+if Rails.env.production?
+  AdminUser.create!(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password')
+end
