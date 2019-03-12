c771b9fbd0d9781cc4b0b79d256ce4ce38d6f028
luftkluft <luftkluft@gmail.com>
2019-03-12 22:49:24 +0300

    Clean mailer rem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,14 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@bs.com'
-  # layout "mailer"
 
   def welcome_email(user)
-    @user = user # params[:user]
+    @user = user
     @url  = 'https://bs-t.herokuapp.com/users/sign_in'
     mail(to: @user.email, subject: 'Welcome to BookStore')
   end
 
   def order_email(user)
-    @user = user # params[:user]
+    @user = user
     mail(to: @user.email, subject: 'Order from BookStore')
   end
 end
