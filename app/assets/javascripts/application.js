1ed766bbb784f53ad31191a8a5b476f06bf774a3
luftkluft <luftkluft@gmail.com>
2019-03-12 22:39:57 +0300

    Refactoring/dev: move js from cart erb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,39 @@
 //= require home
 //= require cart
 //= require_tree .
+
+function delete_item(item_id) {
+  if (confirm("Are you sure?")) {
+    window.location.href = '/delete_item?delete_item=' + item_id;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function change_quantity(click_id, item_id) {
+  var el = document.getElementById('cart-input-item-' + item_id);
+  if (click_id.includes('plus')) {
+    if (el.value <= 14) {
+      el.value = Number(el.value) + 1;
+      $.ajax({
+        type: "GET",
+        url: '/increment',
+        data: { increment: item_id }
+      })
+    };
+  }
+
+  if (click_id.includes('minus')) {
+    if (el.value >= 1) {
+      el.value = Number(el.value) - 1;
+      $.ajax({
+        type: "GET",
+        url: '/decrement',
+        data: { decrement: item_id }
+      })
+    };
+  }
+  return true;
+}
