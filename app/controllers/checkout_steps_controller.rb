2f346d614a5ec8967459e6b199f00cca8d1fa2a3
luftkluft <luftkluft@gmail.com>
2019-08-14 18:10:15 +0300

    Refactoring/dev: set_checkout_step method

--- app/controllers/checkout_steps_controller.rb
+++ app/controllers/checkout_steps_controller.rb
@@ -65,10 +65,10 @@ class CheckoutStepsController < ApplicationController
 
   def set_checkout_step(step)
     @cart.checkout_step = step
-    @cart.save
-  rescue StandardError
-    flash[:alert] = I18n.t('checkout.steps.error_save_state')
-    redirect_back(fallback_location: root_path) && return
+    unless @cart.save
+      flash[:alert] = I18n.t('checkout.steps.error_save_state')
+      redirect_back(fallback_location: root_path) && return
+    end
   end
 
   def choose_delivery
