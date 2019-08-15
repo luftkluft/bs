3f08ec8f79aedd77f4d695af64053e7f5669f8af
luftkluft <luftkluft@gmail.com>
2019-08-15 18:26:27 +0300

    Dev: config assets

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Bookstore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.serve_static_assets = true
     config.assets.enabled = true
     config.assets.paths << Rails.root.join('app', 'assets', 'fallback', 'png')
     config.paths['app/views'] << 'app/views/devise'
