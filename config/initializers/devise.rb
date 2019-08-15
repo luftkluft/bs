fb84e7317db610cf57902c28c825bd9f9239c25f
luftkluft <luftkluft@gmail.com>
2019-08-15 18:58:08 +0300

    Config/dev: devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -287,6 +287,6 @@ Devise.setup do |config|
   # end
   config.secret_key = '-- secret key --'
   #ID first, secret second
-  config.omniauth :facebook, Rails.application.credentials.facebook[:id],
-    Rails.application.credentials.facebook[:secret]
+  ## config.omniauth :facebook, Rails.application.credentials.facebook[:id],
+  ##  Rails.application.credentials.facebook[:secret]
 end
